ecd4e001374b8e6f10430d0b7739eaa3ae0eae6a
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2022-11-14 20:41:42 -0600

    ✅ add tests set up

---
+++ src/components/App/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../index";
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    render(<App />);
+    screen.debug();
+  });
+});
---
+++ src/components/App/index.tsx
@@ -0,0 +1,7 @@
+import "./index.css";
+
+function App() {
+  return <>hello there</>;
+}
+
+export default App;
